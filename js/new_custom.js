3ac70562ebfd77fcb0447b39c93c7b05577c6e8b
sujin <asj9674@gmail.com>
2021-03-28 20:02:59 +0900

    home responsive application

--- js/new_custom.js
+++ js/new_custom.js
@@ -1,12 +1,37 @@
 $(function(){
+    var win_w = $(window).width();
 
+    $(window).on('resize',function(){
+        win_w = $(this).width();
+    });
+
+    // header gnb menu
     $('#gnb>li').on('mouseenter',function(){
-        $(this).children('.submenu').stop(true,true).show();
+        if(win_w >= 980){
+            $(this).children('.submenu').stop(true,true).show();
+        } else {
+            $('#gnb>li').off('click');
+            $('#gnb>li').on('click',function(){
+                $('.submenu').stop().slideUp();
+                $(this).children('.submenu').stop().slideToggle();
+            });
+        }
     });
 
     $('#gnb>li').on('mouseleave',function(){
-        $(this).children('.submenu').stop(true,true).hide();
+        if(win_w >= 980){
+            $(this).children('.submenu').stop(true,true).hide();
+        }
     });
+
+    $('#header .toggle').on('click',function(){
+        $('.nav_wrap , .toggle, .cancle ').addClass('on');
+    });
+    $('#header .cancle').on('click',function(){
+        $('.nav_wrap , .toggle, .cancle ').removeClass('on');
+    });
+
+
 
 
     //work_swiper
@@ -15,7 +40,6 @@ $(function(){
 
         mousewheel: {
             forceToAxis: true,
-
         },
      
         pagination: {
@@ -27,6 +51,16 @@ $(function(){
           prevEl: '.swiper-button-prev',
         },
       
+    });
+
+    $('#gnb>.home').on('click',function(){
+        swiper.slideTo(0, 0.5);
+    });
+    $('#gnb>.work').on('click',function(){
+        swiper.slideTo(1, 0.5);
+    });
+    $('#gnb>.contact').on('click',function(){
+        swiper.slideTo(6, 0.5);
     });
 
 
